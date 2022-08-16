6d6381b6c96955223eebcb90289ce31f36963f76
Anderson V. Dávila Guerrero <avdavilag@utn.edu.ec>
2022-08-16 15:20:27 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '01_manage_class',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
